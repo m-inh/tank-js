cb14d010793958c1f0039eff9ebe7988df7ff0ad
Nguyễn Tiến Minh <minhnt_58@vnu.edu.vn>
2016-06-29 00:42:04 +0700

    change port to 3000

--- index.js
+++ index.js
@@ -207,8 +207,8 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/tank_client/index.html');
 });
 
-http.listen(3001, function () {
-    console.log('listening on *:3001');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
 
 
