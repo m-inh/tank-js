0d6658b2e9c82e23aecd767235f2fb344df02d62
Nguyễn Tiến Minh <tienminh.uet@gmail.com>
2016-10-26 11:33:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -207,8 +207,8 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/tank_client/index.html');
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(80, function () {
+    console.log('listening on *:80');
 });
 
 
