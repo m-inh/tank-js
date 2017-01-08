b9689c5ba5db2353d7b1dc68b8e37543783fefe6
minhnt58 <tienminh.uet@gmail.com>
2017-01-08 18:50:29 +0700

    es6->es5

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
-gulp.task('default', function () {
-
-    // browser source
-    gulp.src("public/es6/*.js")
-        .pipe(babel())
-        .pipe(gulp.dest("public/dist/scripts"));
+gulp.task('default', () => {
+    return gulp.src('public/es6/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('public/dist/scripts'));
 });
